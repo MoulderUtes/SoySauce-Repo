ccac4713d6492d0ec3f6aa7711ef03e4b41f3c1b
MoulderUtes <brycemoulder@gmail.com>
2021-03-04 14:15:03 -0800

    name change

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-rm SoySauce_repo*
+rm SoySauce-repo*
 
 echo "repo-add"
 #repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
-repo-add -s -n -R SoySauce_repo.db.tar.gz *.pkg.tar.zst
+repo-add -s -n -R SoySauce-repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 #cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
 
 echo "####################################"
 echo "Repo Updated!!"
-echo "####################################"
